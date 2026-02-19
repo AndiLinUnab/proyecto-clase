a60ae1e2625930fd9aaccbf1f7500c51270c9789
AndiLinUnab <alin@unab.edu.co>
2026-02-19 11:08:42 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/product', [ ProductController::class, 'index' ]);
-Route::get('/product/create', [ ProductController::class, 'create' ]);
-Route::get('/product/{producto}', [ ProductController::class, 'show']);
+Route::prefix('product')->controller(ProductController::class)->group(function(){
+    Route::get('/', 'index' );
+    Route::get('/create', 'create' );
+    Route::get('/{producto}', 'show');
+});
+
